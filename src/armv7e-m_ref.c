fcb0872cf1e7e28fd000d21d692c4172ae2b5581
vincentvbh <vincentvbh7@gmail.com>
2024-10-04 22:14:42 +0200

    use smlawx

--- src/armv7e-m_ref.c
+++ src/armv7e-m_ref.c
@@ -7,50 +7,28 @@
 
 #define KYBER_Q 3329
 
-int16_t shadd(const int16_t a, const int16_t b){
-    return (a + b) >> 1;
-}
-
-int16_t mul(const int16_t a, const int16_t b){
+int32_t mul(const int32_t a, const int32_t b){
     return a * b;
 }
 
-int16_t mla(const int16_t c, const int16_t a, const int16_t b){
+int32_t mla(const int32_t c, const int32_t a, const int32_t b){
     return c + a * b;
 }
 
-int16_t mls(const int16_t c, const int16_t a, const int16_t b){
+int32_t mls(const int32_t c, const int32_t a, const int32_t b){
     return c - a * b;
 }
 
-int16_t sqdmulh(const int16_t a, const int16_t b){
-    return (int16_t)(((int32_t)a * b * 2) >> 16);
+int32_t smmul(const int32_t a, const int32_t b){
+    return ((int64_t)a * b) >> 32;
 }
 
-int16_t sqrdmulh(const int16_t a, const int16_t b){
-    return (int16_t)(((int32_t)a * b * 2 + (1 << 15)) >> 16);
+int32_t smmulr(const int32_t a, const int32_t b){
+    return ((int64_t)a * b + (1LL << 31)) >> 32;
 }
 
-int16_t srshr(const int16_t a, const size_t i){
-    if(i == 0){
-        return a;
-    }
-    return ((a >> (i - 1)) + 1) >> 1;
-}
-
-int16_t sshr(const int16_t a, const size_t i){
-    if(i == 0){
-        return a;
-    }
-    return a >> i;
-}
-
-int16_t srsra(int16_t c, const int16_t a, const size_t i){
-    return c + srshr(a, i);
-}
-
-int16_t ssra(int16_t c, const int16_t a, const size_t i){
-    return c + sshr(a, i);
+int32_t smlawx(const int32_t c, const int16_t a, const int32_t b){
+    return (((int64_t)a * b) >> 16) + c;
 }
 
 int16_t compress_D(int16_t a, const size_t D){
@@ -64,21 +42,28 @@ int16_t Barrett_quotient_1(int16_t a){
     // 19-bit suffices for D = 1
     // 315 = round(2 * 2^19 / q)
     // return ((int16_t)(((int32_t)a * 315 + (1 << 18)) >> 19)) & 0x1;
-    return srshr(sqdmulh(a, 315), 4) & 0x1;
+    // 2580335 = round(2 * 2^32 / q)
+    return (smlawx(1 << 15, a, 2580335) >> 16) & 0x1;
+    // return smmulr((int32_t)a, 2580335) & 0x1;
 }
 
 int16_t Barrett_quotient_4(int16_t a){
     // 16-bit suffices for D = 4
     // 315 = round(16 * 2^16 / q)
     // return ((int16_t)(((int32_t)a * 315 + (1 << 15)) >> 16)) & 0xf;
-    return shadd(sqdmulh(a, 315), 1) & 0xf;
+    // 20642679 = round(16 * 2^32)
+    return (smlawx(1 << 15, a, 20642679) >> 16) & 0xf;
+    // return smmulr((int32_t)a, 20642679) & 0xf;
+
 }
 
 int16_t Barrett_quotient_5(int16_t a){
     // 15-bit suffices for D = 5
     // 315 = round(32 * 2^15 / q)
     // return ((int16_t)(((int32_t)a * 315 + (1 << 14)) >> 15)) & 0x1f;
-    return sqrdmulh(a, 315) & 0x1f;
+    // 41285357 = round(32 * 2^32 / q)
+    return (smlawx(1 << 15, a, 41285357) >> 16) & 0x1f;
+    // return smmulr((int32_t)a, 41285357) & 0x1f;
 }
 
 int16_t Barrett_quotient_10(int16_t a){
@@ -87,14 +72,16 @@ int16_t Barrett_quotient_10(int16_t a){
     // 22-bit suffices for D = 10
     // 1290167 = round(1024 * 2^22 / q)
     // return ((int16_t)(((int32_t)a * 1290167 + (1 << 21)) >> 22)) & 0x3ff;
-    return srshr((mla(shadd(sqdmulh(a, -20553), 0), a, 20)), 6) & 0x3ff;
+    // 1321131424 = round(1024 * 2^32 / q)
+    return (smlawx(1 << 15, a, 1321131424) >> 16) & 0x3ff;
+    // return smmulr((int32_t)a, 1321131424) & 0x3ff;
 }
 
 int16_t Barrett_quotient_11(int16_t a){
     // 21-bit suffices for D = 11
     // 1290167 = round(2048 * 2^21 / q)
     // return ((int16_t)(((int32_t)a * 1290167 + (1 << 20)) >> 21)) & 0x7ff;
-    return srshr((mla(shadd(sqdmulh(a, -20553), 0), a, 20)), 5) & 0x7ff;
+    return (smlawx(1 << 4, a, 1290167) >> 5) & 0x7ff;
 }
 
 int main(void){
