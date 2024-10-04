9ac97728616ea5b40031d87fabbae53adcf365ee
vincentvbh <vincentvbh7@gmail.com>
2024-10-04 22:50:01 +0200

    draft avx2

---
+++ src/avx2.c
@@ -0,0 +1,258 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+// 1, 4, 5, 10, 11
+
+#define KYBER_Q 3329
+
+int16_t pmullw(const int16_t a, const int16_t b){
+    return a * b;
+}
+
+int16_t pmulhw(const int16_t a, const int16_t b){
+    return (int16_t)(((int32_t)a * b) >> 16);
+}
+
+int16_t psraw(const int16_t a, const size_t i){
+    if(i == 0){
+        return a;
+    }
+    return a >> i;
+}
+
+int16_t pmulhrsw(const int16_t a, const int16_t b){
+    return (int16_t)(( (int32_t)a * b + (1 << 14)) >> 15);
+}
+
+int16_t compress_D(int16_t a, const size_t D){
+    if(a < 0){
+        a += KYBER_Q;
+    }
+    return (int16_t)(( ( ((int32_t)a) << D) + (KYBER_Q / 2) ) / KYBER_Q) & ((1 << D) - 1);
+}
+
+int16_t Barrett_quotient_1(int16_t a){
+    // 19-bit suffices for D = 1
+    // 315 = round(2 * 2^19 / q)
+    // return ((int16_t)(((int32_t)a * 315 + (1 << 18)) >> 19)) & 0x1;
+    return pmulhrsw(pmulhw(a, 315), (1 << 12)) & 0x1;
+    // return psraw(pmulhw(a, 315) + (1 << 2), 3) & 0x1;
+}
+
+int16_t Barrett_quotient_4(int16_t a){
+    // 16-bit suffices for D = 4
+    // 315 = round(16 * 2^16 / q)
+    // return ((int16_t)(((int32_t)a * 315 + (1 << 15)) >> 16)) & 0xf;
+    // return ((int16_t)(((int32_t)a * 630 + (1 << 16)) >> 17)) & 0xf;
+    return pmulhrsw(pmulhw(a, 630), (1 << 14)) & 0xf;
+    // return psraw(pmulhw(a, 630) + (1 << 0), 1) & 0xf;
+
+}
+
+int16_t Barrett_quotient_5(int16_t a){
+    // 15-bit suffices for D = 5
+    // 315 = round(32 * 2^15 / q)
+    // return ((int16_t)(((int32_t)a * 315 + (1 << 14)) >> 15)) & 0x1f;
+    return pmulhrsw(a, 315) & 0x1f;
+}
+
+// 1290167 = -20553 + 20 * 2^16
+
+int16_t Barrett_quotient_10(int16_t a){
+    // this doesn't work
+    // return ((int16_t)(((int32_t)a * 161271 + (1 << 18)) >> 19)) & 0x3ff;
+    // 22-bit suffices for D = 10
+    // 1290167 = round(1024 * 2^22 / q)
+    // return ((int16_t)(((int32_t)a * 1290167 + (1 << 21)) >> 22)) & 0x3ff;
+    return pmulhrsw(pmulhw(a, -20553) + pmullw(a, 20), (1 << 9)) & 0x3ff;
+    // return psraw(pmulhw(a, -20553) + pmullw(a, 20) + (1 << 5), 6) & 0x3ff;
+
+}
+
+int16_t Barrett_quotient_11(int16_t a){
+    // 21-bit suffices for D = 11
+    // 1290167 = round(2048 * 2^21 / q)
+    // return ((int16_t)(((int32_t)a * 1290167 + (1 << 20)) >> 21)) & 0x7ff;
+    return pmulhrsw(pmulhw(a, -20553) + pmullw(a, 20), (1 << 10)) & 0x7ff;
+    // return psraw(pmulhw(a, -20553) + pmullw(a, 20) + (1 << 4), 5) & 0x7ff;
+}
+
+void poly_compress1(uint8_t r[32], const int16_t a[KYBER_N]){
+
+    unsigned int i,j;
+    int16_t u;
+
+    for(i=0;i<KYBER_N/8;i++) {
+        r[i] = 0;
+        for(j=0;j<8;j++) {
+            u = a[8*i+j];
+
+            // 19-bit precision suffices for round(2 x / q)
+            // inputs are in [-q/2, ..., q/2]
+            // 315 = round(2 * 2^19 / q)
+            u = (int16_t)(((int32_t)u * 315 + (1 << 18)) >> 19) & 1;
+
+            // this is equivalent to first mapping to positive
+            // standard representatives followed by
+            // u = ((((uint16_t)u << 1) + KYBER_Q/2)/KYBER_Q) & 1;
+
+            r[i] |= u << j;
+
+        }
+    }
+
+}
+
+void poly_compress4(uint8_t r[128], const int16_t a[KYBER_N]){
+
+    unsigned int j,k;
+    int16_t u;
+
+    uint16_t t[8];
+    for(j=0;j<KYBER_N/8;j++) {
+        for(k=0;k<8;k++) {
+            u  = a[8*j+k];
+
+            // 16-bit precision suffices for round(2^4 x / q)
+            // inputs are in [-q/2, ..., q/2]
+            // 315 = round(16 * 2^16 / q)
+            t[k] = (int16_t)(((int32_t)u * 315 + (1 << 15)) >> 16) & 0xf;
+
+            // this is equivalent to first mapping to positive
+            // standard representatives followed by
+            // t[j] = ((((uint16_t)u << 4) + KYBER_Q/2)/KYBER_Q) & 0xf;
+
+        }
+
+        r[0] = t[0] | (t[1] << 4);
+        r[1] = t[2] | (t[3] << 4);
+        r[2] = t[4] | (t[5] << 4);
+        r[3] = t[6] | (t[7] << 4);
+        r += 4;
+    }
+}
+
+void poly_compress5(uint8_t r[160], const int16_t a[KYBER_N]){
+
+    unsigned int j,k;
+    int16_t u;
+
+    uint16_t t[8];
+    for(j=0;j<KYBER_N/8;j++) {
+        for(k=0;k<8;k++) {
+            u  = a[8*j+k];
+
+            // 15-bit precision suffices for round(2^5 x / q)
+            // inputs are in [-q/2, ..., q/2]
+            // 315 = round(32 * 2^15 / q)
+            t[k] = (int16_t)(((int32_t)u * 315 + (1 << 14)) >> 15) & 0x1f;
+
+            // this is equivalent to first mapping to positive
+            // standard representatives followed by
+            // t[j] = ((((uint32_t)u << 5) + KYBER_Q/2)/KYBER_Q) & 0x1f;
+
+        }
+
+        r[0] = (t[0] >> 0) | (t[1] << 5);
+        r[1] = (t[1] >> 3) | (t[2] << 2) | (t[3] << 7);
+        r[2] = (t[3] >> 1) | (t[4] << 4);
+        r[3] = (t[4] >> 4) | (t[5] << 1) | (t[6] << 6);
+        r[4] = (t[6] >> 2) | (t[7] << 3);
+        r += 5;
+    }
+}
+
+void poly_compress10(uint8_t r[320], const int16_t a[KYBER_N]){
+
+    unsigned int j,k;
+    int16_t u;
+
+    uint16_t t[4];
+    for(j=0;j<KYBER_N/4;j++) {
+        for(k=0;k<4;k++) {
+            u  = a[4*j+k];
+
+            // 22-bit suffices for round(1024 x / q)
+            // inputs are in [-q/2, ..., q/2]
+            // 1290167 = round(1024 * 2^22 / q)
+            t[k] = ((int16_t)(((int32_t)u * 1290167 + (1 << 21)) >> 22)) & 0x3ff;
+
+            // this is equivalent to first mapping to positive
+            // standard representatives followed by
+            // t[k]  = ((((uint32_t)u << 10) + KYBER_Q/2)/ KYBER_Q) & 0x3ff;
+
+        }
+
+        r[0] = (t[0] >> 0);
+        r[1] = (t[0] >> 8) | (t[1] << 2);
+        r[2] = (t[1] >> 6) | (t[2] << 4);
+        r[3] = (t[2] >> 4) | (t[3] << 6);
+        r[4] = (t[3] >> 2);
+        r += 5;
+    }
+}
+
+void poly_compress11(uint8_t r[352], const int16_t a[KYBER_N]){
+
+    unsigned int j,k;
+    int16_t u;
+
+    uint16_t t[8];
+    for(j=0;j<KYBER_N/8;j++) {
+        for(k=0;k<8;k++) {
+            u  = a[8*j+k];
+
+            // 21-bit suffices for round(2048 x / q)
+            // inputs are in [-q/2, ..., q/2]
+            // 1290167 = round(2048 * 2^21 / q)
+            t[k] = ((int16_t)(((int32_t)u * 1290167 + (1 << 20)) >> 21)) & 0x7ff;
+
+            // this is equivalent to first mapping to positive
+            // standard representatives followed by
+            // t[k]  = ((((uint32_t)u << 11) + KYBER_Q/2)/KYBER_Q) & 0x7ff;
+
+        }
+
+        r[ 0] = (t[0] >>  0);
+        r[ 1] = (t[0] >>  8) | (t[1] << 3);
+        r[ 2] = (t[1] >>  5) | (t[2] << 6);
+        r[ 3] = (t[2] >>  2);
+        r[ 4] = (t[2] >> 10) | (t[3] << 1);
+        r[ 5] = (t[3] >>  7) | (t[4] << 4);
+        r[ 6] = (t[4] >>  4) | (t[5] << 7);
+        r[ 7] = (t[5] >>  1);
+        r[ 8] = (t[5] >>  9) | (t[6] << 2);
+        r[ 9] = (t[6] >>  6) | (t[7] << 5);
+        r[10] = (t[7] >>  3);
+        r += 11;
+    }
+}
+
+int main(void){
+
+    for(int16_t i = -1664; i <= 1664; i++){
+        assert(compress_D(i, 1) == Barrett_quotient_1(i));
+    }
+
+    for(int16_t i = -1664; i <= 1664; i++){
+        assert(compress_D(i, 4) == Barrett_quotient_4(i));
+    }
+
+    for(int16_t i = -1664; i <= 1664; i++){
+        assert(compress_D(i, 5) == Barrett_quotient_5(i));
+    }
+
+    for(int16_t i = -1664; i <= 1664; i++){
+        assert(compress_D(i, 10) == Barrett_quotient_10(i));
+    }
+
+    for(int16_t i = -1664; i <= 1664; i++){
+        assert(compress_D(i, 11) == Barrett_quotient_11(i));
+    }
+
+    printf("Test finished!\n");
+
+}
+
